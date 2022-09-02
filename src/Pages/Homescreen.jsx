fee1387bab1bb275bbcf15a37793ce5b4e4887bf
benoit <marliere.benoit59@gmail.com>
2022-09-02 11:22:06 +0200

    E2 terminée

--- src/Pages/Homescreen.jsx
+++ src/Pages/Homescreen.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import CategoryCard from '../Components/CategoryCard';
 
 const Homescreen = () => {
     return (
         <div>
             <h1>La boutique de Benoît Marlière</h1>
+            <CategoryCard />
         </div>
     );
 };
