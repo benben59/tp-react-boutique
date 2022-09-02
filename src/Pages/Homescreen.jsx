f9ce048335d58fc71534626f1e25624512400d56
benoit <marliere.benoit59@gmail.com>
2022-09-02 09:50:41 +0200

    E1 terminée

---
+++ src/Pages/Homescreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homescreen = () => {
+    return (
+        <div>
+            <h1>La boutique de Benoît Marlière</h1>
+        </div>
+    );
+};
+
+export default Homescreen;
