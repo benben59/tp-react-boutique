abc467b72a5f3f63ff6efc65e4c3807e7c540542
benoit <marliere.benoit59@gmail.com>
2022-09-05 11:08:20 +0200

    E3 terminée

---
+++ src/Components/CategoryCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function CategoryCard(props) {
+  return (
+    <div className='col-6'>
+  <div className="card" style={{width: '18rem'}}>
+    <img src={props.img} class="card-img-top" alt={props.user}/>
+    <div className="card-body">
+    <h5 className="card-title">{props.title}</h5>
+      <p className="card-text">{props.text}</p>
+    </div>
+  </div>
+  </div>
+  );
+}
+
+export default CategoryCard;
