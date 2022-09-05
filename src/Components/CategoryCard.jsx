cd875f96df1dc84c8bc10e885ce29176c5cbee39
benoit <marliere.benoit59@gmail.com>
2022-09-05 10:38:52 +0200

    g

--- src/Components/CategoryCard.jsx
+++ src/Components/CategoryCard.jsx
@@ -1,24 +1,18 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const CategoryCard = () => {
+function CategoryCard(props) {
   return (
-    <div>
-      <div className="container">
-        <div className="card">
-          <img
-            src="../../assets/img/9782811618124-T.jpg"
-            alt="Category"
-          />
-          <h4>Category title</h4>
-
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque,
-            perspiciatis. Consequatur, sit voluptas.
-          </p>
-        </div>
-      </div>
+    <div className='col-6'>
+  <div className="card" style={{width: '18rem'}}>
+    <img src={props.img} class="card-img-top" alt={props.user}/>
+    <div className="card-body">
+    <h5 className="card-title">{props.title}</h5>
+      <p className="card-text">{props.text}</p>
     </div>
+  </div>
+  </div>
   );
-};
+}
 
 export default CategoryCard;
